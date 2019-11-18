0be25ea86e0d1da8404c98041a857adacc55f4de
Shawn Huang Fernandes <shawn.huang.fernandes@gmail.com>
2019-11-18 12:57:36 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+let mainItem = document.getElementById('main');
+mainItem.remove();
+
+let newHeader = document.createElement('h1');
+newHeader.innerHTML = ('YOUR-NAME is the champion');
+newHeader.className = "victory";
+newHeader.id = 'victory';
+document.body.appendChild(newHeader);
