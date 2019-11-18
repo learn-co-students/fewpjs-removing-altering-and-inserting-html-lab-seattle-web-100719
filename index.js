8bca28d6c7099c43787c1384b5f5c3801c71f99b
Rachid Rakaa <rachid1982fsb@gmail.com>
2019-11-18 15:00:10 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your code here!
+let element = document.createElement('div')
+
+document.body.appendChild(element)
+
+let ul = document.createElement('ul')
+ 
+for (let i = 0; i < 3; i++) {
+  let li = document.createElement('li')
+  li.innerHTML = (i + 1).toString()
+  ul.appendChild(li)
+}
+ 
+element.appendChild(ul)
+
+// ul.removeChild(ul.querySelector('li:nth-child(2)'))
+// ul.remove()
+document.querySelector('main').remove()
+let newHeader = document.createElement('h1')
+document.body.appendChild(newHeader)
+newHeader.id = 'victory'
+newHeader.innerHTML = "YOUR-NAME is the champion"
